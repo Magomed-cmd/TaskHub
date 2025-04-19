0a11c945e6d245b65090953eadfc98141b5f34c8
Magomed-cmd <maga_valiev@inbox.ru>
2025-04-19 19:16:07 +0300

    feat: добавлена директория router, тестовый роут /ping, настройка Engine

--- cmd/taskhub/main.go
+++ cmd/taskhub/main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"TaskHub/internal/config"
+	"TaskHub/internal/server"
 	"log"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
 	cfg, err := config.LoadConfig()
-	if err != nil{
-		log.Fatalln(err)	
+	if err != nil {
+		log.Fatalln(err)
 	}
 
-	log.Println(&cfg)
+	router := server.New()
 
+
+	router.GET("/ping", handler)
+
+	router.Run(":" + cfg.PORT, )
+
+}
+
+
+func handler(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"message": "pong"})
 }
